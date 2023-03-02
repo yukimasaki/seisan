f51cdca36f6d028647defaff9073a1c9396d241a
yukimasaki <woodenhoe@gmail.com>
2023-03-02 12:23:01 +0000

    Vuetify3動作確認

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-main id="app">
+      <v-container>
+        <v-card>
+          <v-card-title>Hello Vuetify3!!</v-card-title>
+          <v-card-subtitle>Provide by Nuxt3</v-card-subtitle>
+          <v-card-text>This is test page.</v-card-text>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
