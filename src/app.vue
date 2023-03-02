65eaa45287fa18cb11b3f21de9243d84064585b2
yukimasaki <woodenhoe@gmail.com>
2023-03-02 12:35:57 +0000

    TypeScript動作確認

--- src/app.vue
+++ src/app.vue
@@ -3,7 +3,7 @@
     <v-main id="app">
       <v-container>
         <v-card>
-          <v-card-title>Hello Vuetify3!!</v-card-title>
+          <v-card-title>{{ pageTitle }}</v-card-title>
           <v-card-subtitle>Provide by Nuxt3</v-card-subtitle>
           <v-card-text>This is test page.</v-card-text>
         </v-card>
@@ -11,3 +11,7 @@
     </v-main>
   </v-app>
 </template>
+
+<script setup lang="ts">
+const pageTitle: string = 'Hello Vuetify3!'
+</script>
