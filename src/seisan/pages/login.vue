0e8f303fe0d39e470c987ae46b4d3d2ff55432a2
yukimasaki <woodenhoe@gmail.com>
2023-03-06 06:55:23 +0000

    ログイン、ユーザー登録ページ作成

---
+++ src/seisan/pages/login.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" sm="12" md="4">
+      <v-card>
+        <v-card-title class="justify-center" mb-3>
+          <h1 class="display-1">
+            ログイン
+          </h1>
+        </v-card-title>
+
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              v-model="email"
+              label="メールアドレス"
+            />
+            <v-text-field
+              v-model="password"
+              label="パスワード"
+              type="password"
+            />
+            <nuxt-link to="/register">
+              ユーザー登録
+            </nuxt-link>
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions class="justify-center">
+          <v-btn @click="login">
+            ログイン
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    async login () {
+
+    }
+  }
+}
+</script>
---
+++ src/seisan/pages/register.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" sm="12" md="4">
+      <v-card>
+        <v-card-title class="justify-center" mb-3>
+          <h1 class="display-1">
+            ユーザー登録
+          </h1>
+        </v-card-title>
+
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              v-model="email"
+              label="メールアドレス"
+            />
+            <v-text-field
+              v-model="username"
+              label="ユーザー名"
+            />
+            <v-text-field
+              v-model="password"
+              label="パスワード"
+              type="password"
+            />
+            <v-text-field
+              v-model="passwordRepeat"
+              label="パスワード(再入力)"
+              type="password"
+            />
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions class="justify-center">
+          <v-btn @click="register">
+            登録
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'RegisterPage',
+
+  data () {
+    return {
+      email: '',
+      password: '',
+      passwordRepeat: '',
+      username: ''
+    }
+  },
+
+  methods: {
+    async register () {
+
+    }
+  }
+}
+</script>
