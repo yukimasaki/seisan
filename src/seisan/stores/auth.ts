d6f3b53a461b8546cee833e2e23f4dd0ec9387f1
yukimasaki <woodenhoe@gmail.com>
2023-03-08 06:48:10 +0000

    store/auth作成

---
+++ src/seisan/stores/auth.ts
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia'
+
+export const useStoreAuth = defineStore({
+  id: 'auth',
+
+  state: () => ({
+    user: null,
+  }),
+
+  getters: {
+    isLoggedIn: (state) => state.user,
+  },
+
+  actions: {
+    setUser(user) {
+      this.user = user
+    },
+    clearUser() {
+      this.user = null
+    },
+  },
+})
