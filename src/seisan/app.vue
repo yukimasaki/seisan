b4b13dc58a4a2fa1e6c71d9c6a0445f097e4b14d
yukimasaki <woodenhoe@gmail.com>
2023-03-08 05:15:37 +0000

    Vuetify, mdiインストール

--- src/seisan/app.vue
+++ src/seisan/app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>
+        Nuxt App
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <NuxtPage />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
