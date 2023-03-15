9774d2b8443da24fbad05a38cb3e6fa7d46bfeef
yukimasaki <woodenhoe@gmail.com>
2023-03-15 08:19:29 +0000

    fix: ログイン後にトップページへリダイレクト

--- src/seisan/middleware/authenticated.ts
+++ src/seisan/middleware/authenticated.ts
@@ -5,19 +5,9 @@ export default defineNuxtRouteMiddleware((to, _from) => {
   const user = useSupabaseUser()
   const route = useRoute()
 
-  const rules = [
-    route.path == '/register' ? true : false,
-    route.path == '/login' ? true : false
-  ]
-
-  /** ISSUE: ログイン後にrules記載のURLでリロードすると
-   *  下記処理が正当に評価されているにもかかわらずトップページへリダイレクトされない
-   */
-  if (user.value != null) {
-    rules.some(v => {
-      if (v == true) {
-        return navigateTo('/')
-      }
-    })
+  if (user.value) {
+    if (route.path == '/register' || route.path == '/login') {
+      return navigateTo('/')
+    }
   }
 })
