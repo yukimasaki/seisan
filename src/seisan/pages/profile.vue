77e46bc2b0cafe4c9797643d0e233c3d6a939961
yukimasaki <woodenhoe@gmail.com>
2023-03-16 14:13:29 +0000

    TODO: アバター画像編集ボタンを実装中

--- src/seisan/pages/profile.vue
+++ src/seisan/pages/profile.vue
@@ -10,11 +10,17 @@
             <div class="mt-auto mb-3 text-center">
               <v-avatar
                 size="180"
-              >
+                >
                 <v-img
-                  src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=300:*"
-                  cover
-                />
+                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=300:*"
+                cover
+                style="position: relative;"
+                >
+                  <v-sheet
+                    height="100%"
+                    style="position: absolute;"
+                  />
+                </v-img>
               </v-avatar>
             </div>
 
