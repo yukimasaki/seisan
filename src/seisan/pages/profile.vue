ac03b6323e615f67246fd28fdbb1781fa1ec2e1d
yukimasaki <woodenhoe@gmail.com>
2023-03-17 02:19:06 +0000

    Supabase DB接続テストを実施中

--- src/seisan/pages/profile.vue
+++ src/seisan/pages/profile.vue
@@ -10,17 +10,17 @@
             <div class="mt-auto mb-3 text-center">
               <v-avatar
                 size="180"
-                >
-                <v-img
-                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=300:*"
-                cover
                 style="position: relative;"
                 >
-                  <v-sheet
-                    height="100%"
-                    style="position: absolute;"
-                  />
+                <v-img
+                  src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=300:*"
+                  cover
+                  style="position: absolute;"
+                  >
                 </v-img>
+                <div class="avatar-footer" @click="changeAvatar">
+                  <p>変更</p>
+                </div>
               </v-avatar>
             </div>
 
@@ -35,7 +35,44 @@
 </template>
 
 <script setup>
+import { client } from 'process';
+
   definePageMeta({
     middleware: 'require-auth'
   })
+
+  const changeAvatar = () => {
+    console.log(`clicked`)
+  }
+
+  onMounted(async () => {
+    const user = useSupabaseUser()
+    console.log(user)
+
+    const dbClient = useSupabaseClient()
+    const { data: profiles } = await useAsyncData('profiles', async () => {
+      const { data } = await dbClient.from('profiles').select('username').eq('id', user.id).single()
+      return data
+    })
+    console.log(profiles)
+  })
 </script>
+
+<style>
+.avatar-footer {
+  position: absolute;
+  display: table;
+  bottom: 0;
+  background-color: rgba(0,0,0,.4);
+  min-width: 100%;
+  min-height: 20%;
+  cursor: pointer;
+}
+
+.avatar-footer p {
+  display: table-cell;
+  vertical-align: middle;
+  color: white;
+  padding-bottom: 0.25rem;
+}
+</style>
