a68d475e21b0e9b56e6da15fbbf50fe2faaf2ee0
yukimasaki <woodenhoe@gmail.com>
2023-03-20 18:09:03 +0000

    dotenv設定追記

--- src/seisan/nuxt.config.ts
+++ src/seisan/nuxt.config.ts
@@ -10,5 +10,12 @@ export default defineNuxtConfig({
   ],
   imports: {
     dirs: ['stores']
-  }
+  },
+  runtimeConfig: {
+    public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || '',
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || '',
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || '',
+    },
+  },
 })
