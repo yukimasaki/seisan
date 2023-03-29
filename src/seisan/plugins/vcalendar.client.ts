fc9dff48ab4b69ce4c28f82c8a23ba2c9cdf46fe
yukimasaki <woodenhoe@gmail.com>
2023-03-29 04:35:54 +0000

    style.cssをインポート

--- src/seisan/plugins/vcalendar.client.ts
+++ src/seisan/plugins/vcalendar.client.ts
@@ -1,8 +1,8 @@
 /** Nuxt3におけるプラグインの読み込み方法
  *  https://tech.torico-corp.com/blog/migrate-nuxt2-to-nuxt3-on-our-product/
  */
-import {defineNuxtPlugin} from 'nuxt/app'
 import VCalendar from 'v-calendar'
+import 'v-calendar/style.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VCalendar, {})
