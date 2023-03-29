492720d41c342db521cf60a93d45d4500b1ad935
yukimasaki <woodenhoe@gmail.com>
2023-03-29 04:53:13 +0000

    vuetifyカスタムテーマを実装

--- src/seisan/plugins/vuetify.ts
+++ src/seisan/plugins/vuetify.ts
@@ -2,10 +2,33 @@ import { createVuetify } from "vuetify"
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 
+/** カスタムテーマをヘルパーを使用して読み込む方法
+ *  https://zenn.dev/coedo/articles/nuxt3-vuetify3
+ */
+import { defaults } from '@/helpers/defaults'
+import { MAIN_THEME, mainTheme, MAIN_DARK_THEME, mainDarkTheme } from '@/helpers/themes'
+
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
+    defaults,
     components,
     directives,
+    display: {
+      mobileBreakpoint: 'sm',
+    },
+    theme: {
+      defaultTheme: MAIN_THEME,
+      themes: {
+        mainTheme,
+        mainDarkTheme,
+      },
+      // primary-darken-9 primary-lighten-9 までできるようにする
+      variations: {
+        colors: ['primary', 'secondary', 'accent'],
+        lighten: 9,
+        darken: 9,
+      },
+    },
   })
 
   nuxtApp.vueApp.use(vuetify);
