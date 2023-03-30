3c50a69347693c5c851cd35a3ab0cab7532d93b6
yukimasaki <woodenhoe@gmail.com>
2023-03-30 05:37:51 +0000

    シングルクオーテーションに置換

--- src/seisan/composables/useFormatDate.ts
+++ src/seisan/composables/useFormatDate.ts
@@ -9,7 +9,7 @@ const useFormatDate = () => {
     const year = date.getFullYear()
     const month = date.getMonth() + 1 // getMonth()メソッドは0から始まるため、1を足して月を取得する
     const day = date.getDate()
-    const weekday = ["日", "月", "火", "水", "木", "金", "土"][date.getDay()] // 曜日の配列
+    const weekday = ['日', '月', '火', '水', '木', '金', '土'][date.getDay()] // 曜日の配列
     const zeroPad = (num) => num < 10 ? `0${num}` : num // 1桁の数値を2桁に変換する関数
 
     // フォーマット文字列に合わせて日付文字列を生成する
